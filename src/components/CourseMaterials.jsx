70909c2570b4002859eacc86f8ab79e763a96cd9
Aaliyah Loechner <aaliyahloechner@gmail.com>
2023-11-15 01:23:02 -0500

    latest attempt at routing within courses (not currently working correctly)

---
+++ src/components/CourseMaterials.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CourseMaterials() {
+  return (
+    <div>
+      <h2>CourseMaterials</h2>
+      {/* Add specific content for CourseMaterials */}
+    </div>
+  );
+}
+
+export default CourseMaterials;
